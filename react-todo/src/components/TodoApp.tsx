6fe6a408b9e719a2ea4dc84e51556e9bf17d5c47
RuhamaTesfaye <32181019+RuhamaTesfaye@users.noreply.github.com>
2025-04-04 23:02:45 +0000

    input field added for the task

--- react-todo/src/components/TodoApp.tsx
+++ react-todo/src/components/TodoApp.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import TodoList from "./TodoList";
 
 function TodoApp() {
-    const tasks= ["buy milk", "do homework", "Call mommy"];
+    // const tasks= ["buy milk", "do homework", "Call mommy"];
+    const[tasks, setTasks]=useState<string[]>(["eat chipotle",]);
+    const[taskInput, setTaskInput]=useState<string>('');
     return (
         <div>
-            <h1>Todo App </h1>
-            <h2>Todo List:</h2>
+            <h1>Todo List:</h1>
+            <input
+           value={taskInput}
+           onChange= {(e)=>setTaskInput(e.target.value)
+
+           } />
            <TodoList items={tasks}/>
         </div>
 
